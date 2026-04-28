2022d4ca9d2259d7de38f0671e86e0665fddf9ff
Mahek Imtiyaz <maherimtiyaz7@gmail.com>
2026-04-28 15:00:24 +0530

    Fixes bugs in main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 # Create app and connects routes
 from fastapi import FastAPI
-from app.routes import users, items
+from app.routes import user, document, processing
+from app.routes.document import router as document_router
+from app.routes.processing import router as processing_router
+
 
 app = FastAPI()
 
-app.include_router(users.router)
-app.include_router(items.router)
+app.include_router(user.router)
+app.include_router(document.router)
+app.include_router(processing.router)
