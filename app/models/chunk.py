2df19b4c69b6975ba8bc63e1b2ead30e579bdee4
Mahek Imtiyaz <maherimtiyaz7@gmail.com>
2026-04-28 15:07:18 +0530

    Fixes models chunks bugs facing issues of imports

--- app/models/chunk.py
+++ app/models/chunk.py
@@ -1,10 +1,18 @@
 # Data Models
+from tokenize import String
+
+
+from sqlalchemy import Column, String, Integer, ForeignKey
 from pydantic import BaseModel, Field
 from datetime import datetime
+from app.db.database import Base
 
-class Chunk(BaseModel):
-    id: str
-    document_id: str
-    content: str
-    chunk_index: int
-    created_at: datetime = Field(default_factory=datetime.utcnow)
+class Chunk(Base):
+    __tablename__ = "chunks"
+    __allow_unmapped__ = True
+
+    id = Column(String, primary_key=True)
+    document_id = Column(Integer, index=True)
+    content = Column(String)
+    chunk_index = Column(Integer)
+    created_at = Column(String, default=datetime.utcnow)
